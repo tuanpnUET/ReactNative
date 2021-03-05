f1358b12f1ad0634e150e4962223c2f7346046bf
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-03-05 16:32:09 +0700

    Update App.js

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container, {padding: 100}}>
+      <View>
+        <TextInput
+          style={styles.TextInput}
+          placeholder ="Enter to do item"/>
+          <Button title ="Add to do"
+          onPress = {() => console.log("Button clicked")}
+          />
+      </View>
+      <View>
+        <Text>List of Todo</Text>
+      </View>
+      {/* <Text>Hello World!</Text> */}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
  
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    padding : 60
+
   },
+  TextInput: {
+    padding: 10,
+    borderColor: 'black',
+    marginBottom: 10,
+    borderWidth:  1
+  }
 });
