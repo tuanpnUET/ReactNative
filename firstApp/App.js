5034888c466a2d9d265dfeb9cc980b45d76c66c9
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-03-05 16:10:40 +0700

    add

--- firstApp/App.js
+++ firstApp/App.js
@@ -5,12 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
