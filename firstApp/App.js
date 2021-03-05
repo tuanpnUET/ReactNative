6c27818de610c45b3df217882d8fa09c423f290b
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-03-05 17:43:39 +0700

    Update App.js

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,23 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Item, SafeAreaView, FlatList } from 'react-native';
 
 export default function App() {
+  const [todoItem, setTodoItem] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodoList = () => {
+    setTodoList(...todoList, todoItem);
+    console.log(todoList);
+  }
+  const DATA = [
+    {
+      id: '1',
+      title: 'First Item',
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item',
+    }
+  ];
+  const Item = ({title}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+  const renderItem = ({item}) => (
+    <Item title={item.title}/>
+  );
+
   return (
-    <View style={styles.container, {padding: 100}}>
+    <View style={styles.container}>
       <View>
         <TextInput
-          style={styles.TextInput}
-          placeholder ="Enter to do item"/>
-          <Button title ="Add to do"
-          onPress = {() => console.log("Button clicked")}
-          />
+          style={styles.textInput}
+          placeholder="Enter todo item"
+          onChangeText={text => setTodoItem(text)}
+          value={todoItem}
+        />
+          <Button 
+          title="Add to do"
+          // onPress = {() => console.log("Button clicked")}
+          onPress={addTodoList}
+        />
       </View>
-      <View>
-        <Text>List of Todo</Text>
-      </View>
-      {/* <Text>Hello World!</Text> */}
-      {/* <StatusBar style="auto" /> */}
+      {/* <ScrollView>
+        {todoList.map(todo => <View style={styles.todoItem}><Text>{todo}</Text></View>)}
+      </ScrollView> */}
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        ></FlatList>
+      </SafeAreaView>
+
     </View>
   );
 }
@@ -31,10 +72,29 @@ const styles = StyleSheet.create({
     padding : 60
 
   },
-  TextInput: {
+  textInput: {
     padding: 10,
     borderColor: 'black',
     marginBottom: 10,
     borderWidth:  1
+  },
+  todoItem: {
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 20,
+  },
+  item: {
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray'
   }
 });
